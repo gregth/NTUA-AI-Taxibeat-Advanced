a6b8f3655a06a8b7b9474f43eeddfbf221bf8543
Themis Papameletiou <themicp@gmail.com>
2017-12-02 15:50:09 +0200

    Housekeeping

--- Taxibeat.java
+++ Taxibeat.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Taxibeat {
-    public static void main(String args[]) throws IOException {
+    public static void main() {
         ArrayList<Taxi> fleet = ParseTaxis.parse();
         Position clientPosition = ParseClient.parse();
 
