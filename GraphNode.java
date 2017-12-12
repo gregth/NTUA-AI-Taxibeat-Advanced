ecbe12bef8006a0de714c37aa9fc217637ca140f
Themis Papameletiou <themis@reembed.com>
2017-12-12 18:43:50 +0200

    [GRAPHNODE]: Add support for equals() and hashCode() to use it as HashMap key

--- GraphNode.java
+++ GraphNode.java
@@ -1,14 +1,36 @@
+import java.util.*;
+
 public class GraphNode {
-    private String position;
-    private double weight;
-    private double h;
+    private Position position;
     private boolean goal = false;
     private boolean visited = false;
 
-    public GraphNode(String position, double weight, double h) {
+    @Override
+    public boolean equals(Object b) {
+        // self check
+        if (this == b) {
+            return true;
+        }
+        // null check
+        if (b == null) {
+            return false;
+        }
+        // type check and cast
+        if (getClass() != b.getClass()) {
+            return false;
+        }
+
+        GraphNode node = (GraphNode) b;
+        return position.distanceTo(node.getPosition()) == 0; 
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position.getX(), position.getY());
+    }
+
+    public GraphNode(Position position) {
         this.position = position;
-        this.weight = weight;
-        this.h = h;
     }
 
     public boolean isVisited() {
@@ -19,7 +41,7 @@ public class GraphNode {
         visited = true;
     }
 
-    public String getPosition() {
+    public Position getPosition() {
         return position;
     }
 
@@ -31,12 +53,8 @@ public class GraphNode {
         return goal;
     }
 
-    public double getTotalWeight() {
-        return weight + h;
-    }
-
     public void print() {
-        System.out.println("('" + this.position + "', " + this.weight + ", " + this.h + ", " + this.goal + ")");
+        System.out.println("('" + this.position.stringify() + "', " + this.goal + ")");
     }
 }
 
