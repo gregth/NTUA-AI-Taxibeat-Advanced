43901c2f464416267470b66a7de99fbc3ec74e0e
Themis Papameletiou <themicp@gmail.com>
2017-12-12 02:20:41 +0200

    [GRAPHNODE]: Add heuristic value, visited and goal flags in class

--- GraphNode.java
+++ GraphNode.java
@@ -1,14 +1,42 @@
 public class GraphNode {
     private String position;
     private double weight;
+    private double h;
+    private boolean goal = false;
+    private boolean visited = false;
 
-    public GraphNode(String position, double weight) {
+    public GraphNode(String position, double weight, double h) {
         this.position = position;
         this.weight = weight;
+        this.h = h;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited() {
+        visited = true;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setGoal() {
+        goal = true;
+    }
+
+    public boolean isGoal() {
+        return goal;
+    }
+
+    public double getTotalWeight() {
+        return weight + h;
     }
 
     public void print() {
-        System.out.println("('" + this.position + "', " + this.weight + ")");
+        System.out.println("('" + this.position + "', " + this.weight + ", " + this.h + ", " + this.goal + ")");
     }
 }
 
