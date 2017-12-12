15bffbaa12acc41d9f54bb8e3080c0c572156fa2
Themis Papameletiou <themicp@gmail.com>
2017-12-12 02:21:49 +0200

    [TAXIBEAT]: Add method that searches in the search space for the client

--- Taxibeat.java
+++ Taxibeat.java
@@ -20,5 +20,47 @@ public class Taxibeat {
         myWorld.clientPosition.print();
         String[] test = {"test"};
         XMLFile.getInstance().write(test);
+        solve(searchSpace, driverPosition);
+    }
+
+    private static void solve(HashMap<String, ArrayList<GraphNode>> searchSpace, Position startPosition) {
+        Comparator<GraphNode> comparators = new GraphNodeComparator();
+        PriorityQueue<GraphNode> queue = new PriorityQueue<GraphNode>(comparators);
+
+        String startPositionString = startPosition.stringify();
+        for (GraphNode neighbor : searchSpace.get(startPositionString)) {
+            queue.add(neighbor);
+        }
+
+        GraphNode top;
+        while (queue.size() > 0) {
+            top = queue.poll();
+            if (top.isGoal()) {
+                System.out.println("FOUND THE CLIENT!!!");
+                break;
+            }
+
+            for (GraphNode neighbor : searchSpace.get(top.getPosition())) {
+                if (!neighbor.isVisited()) {
+                    queue.add(neighbor);
+                    neighbor.setVisited();
+                }
+            }
+        }
+    }
+}
+
+class GraphNodeComparator implements Comparator<GraphNode> {
+    @Override
+    public int compare(GraphNode a, GraphNode b) {
+        if (a.getTotalWeight() < b.getTotalWeight()) {
+            return -1;
+        }
+
+        if (a.getTotalWeight() > b.getTotalWeight()) {
+            return 1;
+        }
+
+        return 0;
     }
 }
