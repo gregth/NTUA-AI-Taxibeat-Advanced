2899ab3f7bcf0a15c6731cc33c0d893e6a81e3cd
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-02-23 00:20:13 +0200

    Fixed processing of lines input

--- Line.java
+++ Line.java
@@ -11,8 +11,8 @@ public class Line {
             PrologParser prologSystem = PrologParser.getInstance();
 
             int id, lanes, maxspeed;
-            String line, highway, name, oneway, boundary, access, natural, barrier, incline, waterway, busway;
-            boolean lit, railway, tunnel, bridge, toll;
+            String line, highway, name, oneway, boundary, access, natural, barrier,
+                   incline, waterway, busway, lit, railway, tunnel, bridge, toll;
             String[] parts = null;
 
             reader.readLine(); // skip the first line with the captions
@@ -28,27 +28,26 @@ public class Line {
                 }
 
                 id = Integer.valueOf(parts[0]);
-                highway = parts[1];
+                highway = parts[1].equals("no") ? "unknown" : parts[1];
                 name = parts[2];
                 oneway = parts[3];
-                lit = parts[4].equals("yes") ? true : false;
+                lit = parts[4];
                 lanes = !parts[5].equals("no") ? Integer.valueOf(parts[5]) : 0;
                 maxspeed = !parts[6].equals("no") ? Integer.valueOf(parts[6]) : 0;
-                railway = parts[7].equals("yes") ? true : false;
+                railway = parts[7];
                 boundary = parts[8];
                 access = parts[9];
                 natural = parts[10];
                 barrier = parts[11];
-                tunnel = parts[12].equals("yes") ? true : false;
-                bridge = parts[13].equals("yes") ? true : false;
+                tunnel = parts[12];
+                bridge = parts[13];
                 incline = parts[14].replace('%', ' ');
                 waterway = parts[15];
                 busway = parts[16];
-                toll = parts[17].equals("yes") ? true : false;
+                toll = parts[17];
 
                 String predicate = "lineSpecs(" + id + "," + highway + "," + "thename" + "," + oneway + "," + lit + "," + lanes + "," + maxspeed + "," + railway + "," + boundary + "," + access + "," + natural + "," + barrier + "," + tunnel + "," + bridge + "," + incline + "," + waterway + "," + busway + "," + toll + ")";
                 prologSystem.asserta(predicate);
-
             }
         } catch (IOException e) {
             System.err.println("Exception:" + e.toString());
