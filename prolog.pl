c8e05a0ebb2e6952606df0f3124a509209c268c3
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-02-23 00:36:59 +0200

    New rules for permited way

--- prolog.pl
+++ prolog.pl
@@ -20,25 +20,40 @@
 /* Holds true if line specified by LineID is not a railway, boundary, access prohibited,
  * natural, barrier, waterway.
  */ 
-isPermittedWay(LineID) :- 
-    lineSpecs(LineID, _, _, _, _, _, _, no, no, no, no, no, _, _, _, no, _, _).
-    
+
+permittedWayType(highway).
+permittedWayType(living_street).
+permittedWayType(motorway).
+permittedWayType(motorway_link).
+permittedWayType(primary).
+permittedWayType(primary_link).
+permittedWayType(residential).
+permittedWayType(secondary).
+permittedWayType(secondary_link).
+permittedWayType(tertiary).
+permittedWayType(trunk).
+permittedWayType(unknown).
+permittedWayType(unclassified).
+
 /* The following predicates hold true if the direction specified is allowed */
 
 /* Holds true if two way line */
 allowedDirection(LineID, _, _) :- 
-    lineSpecs(LineID, _, _, OneWayValue, _, _, _, _, _, _, _, _, _, _, _, _, _, _), 
-    OneWayValue = no.   
+    lineSpecs(LineID, Type, _, OneWayValue, _, _, _, no, no, no, no, no, _, _, _, no, _, _),
+    permittedWayType(Type),
+    OneWayValue = no.
 
 /* If oneway line, determine if movement is allowed using the file line values (FLa, FLb) */
 allowedDirection(LineID, FLa, FLb) :- 
-    lineSpecs(LineID, _, _, OneWayValue, _, _, _, _, _, _, _, _, _, _, _, _, _, _), 
+    lineSpecs(LineID, Type, _, OneWayValue, _, _, _, no, no, no, no, no, _, _, _, no, _, _),
+    permittedWayType(Type),
     OneWayValue = yes, 
     FLa < FLb.
 
 allowedDirection(LineID, FLa, FLb) :- 
-    lineSpecs(LineID, _, _, OneWayValue, _, _, _, _, _, _, _, _, _, _, _, _, _, _), 
+    lineSpecs(LineID, Type, _, OneWayValue, _, _, _, no, no, no, no, no, _, _, _, no, _, _),
     OneWayValue = -1, 
+    permittedWayType(Type),
     FLa > FLb.
 
 canMoveFromTo(Ax, Ay, Bx, By) :-
